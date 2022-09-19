92db4f30fcf5eac581d46e5cc0ed105b931e0aca
jtabalon <joseph@tabalon.io>
2022-09-19 16:48:56 -0700

    add macros

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
+#[derive(Parser)]
 struct Cli {
     pattern: String, 
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::parse();
 }
